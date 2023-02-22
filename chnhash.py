811ed058774a2c1d3dbe9472013446051b927245
Himashana <himashanasuraweera5@gmail.com>
2023-02-22 16:43:08 +0530

    New: app updater

--- chnhash.py
+++ chnhash.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 import sys
-from filehash import FileHash
 import compareHash
+import appUpdater
 
+version = "v0.0.0"
 
 print("\nchnhash -v0.0.1 - CHN Software Developers(https://chnsoftwaredevelopers.com)\n")
 
+updateDetails = appUpdater.isUpdateAvailable(version)
+
+if(updateDetails[0] == True):
+    print("-" * 60)
+    print("A new update is available. Please run the following commands to update this tool to the latest version.")
+    print("wget https://github.com/CHN-Software-Developers/chnhash/archive/refs/tags/" + updateDetails[1] + ".zip")
+    print("or download the zip file manually via the above link.")
+    print("\nUnzip the downloaded zip file and run the setup.sh file to perform the installation.")
+    print("sudo sh setup.sh")
+    print("-" * 60 + "\n")
 try:
     task = sys.argv[1]
     algorithm = sys.argv[2]
